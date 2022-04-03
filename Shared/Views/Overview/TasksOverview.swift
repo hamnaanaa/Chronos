74fde5fe7b1507f2c37c578fc38e91fbe5856bab
Mohammad Naanaa <hamudi.naanaa@tum.de>
2022-04-03 11:56:55 +0200

    Introduce a button to add tasks to the tasks table

--- Shared/Views/Overview/TasksOverview.swift
+++ Shared/Views/Overview/TasksOverview.swift
@@ -17,6 +17,9 @@ struct TasksOverview: View {
     var body: some View {
         ScrollView {
             LazyVStack(alignment: .leading, spacing: 0) {
+                AddTaskButton()
+                Spacer()
+                    .frame(height: TasksTableConstraints.cellHeight / 2)
                 TasksTableHeader(sortingPredicate: $sortingPredicate)
                     .frame(height: TasksTableConstraints.cellHeight)
                 ForEach($tasksContainer.tasks.sorted(by: sortingPredicate)) { $task in
@@ -27,6 +30,60 @@ struct TasksOverview: View {
                 }
             }
             .padding([.horizontal, .top])
+        }
+    }
+    
+    struct AddTaskButton: View {
+        @EnvironmentObject var tasksContainer: TasksContainer
+        
+        /// A flag for displayed an edit sheet for a new `Task`
+        @State private var showingAddSheet = false
+        /// An empty `Epic` to be edited if the `AddTaskButton` is pressed
+        @State private var newTask = Task(title: "", status: .notStarted, category: .organisation, epics: [], description: "", dateDue: nil, dateCreated: .now)
+        
+        var body: some View {
+            Button {
+                showingAddSheet.toggle()
+            } label: {
+                HStack {
+                    Spacer()
+                    Image(systemName: "plus")
+                    Text("New Task")
+                    Spacer()
+                }
+                .font(.headline)
+                .frame(height: TasksTableConstraints.cellHeight)
+                .foregroundColor(.TextColor.blue)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.BackgroundColor.gray)
+                )
+            }
+            .sheet(isPresented: $showingAddSheet) {
+                NavigationView {
+                    TaskEditView(task: $newTask)
+                        .toolbar {
+                            ToolbarItem(placement: .cancellationAction) {
+                                // hide the sheet and reset the new task
+                                Button("Dismiss", role: .cancel) {
+                                    showingAddSheet = false
+                                    newTask = Task(title: "", status: .notStarted, category: .organisation, epics: [], description: "", dateDue: nil, dateCreated: .now)
+                                }
+                                .buttonStyle(.bordered)
+                            }
+                            ToolbarItem(placement: .confirmationAction) {
+                                Button("Add") {
+                                    tasksContainer.addTask(task: newTask)
+                                    showingAddSheet = false
+                                    newTask = Task(title: "", status: .notStarted, category: .organisation, epics: [], description: "", dateDue: nil, dateCreated: .now)
+                                }
+                                .buttonStyle(.bordered)
+                            }
+                        }
+                        .navigationBarTitleDisplayMode(.inline)
+                        .navigationViewStyle(.stack)
+                }
+            }
         }
     }
 }
